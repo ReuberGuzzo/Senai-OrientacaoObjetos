d6b4cadac98ad48100ee392d0c25653be1dd63b7
Reuber Guzzo <reuber.rg@gmail.com>
2022-09-13 21:00:38 -0300

    Closed: #1

---
+++ server/AplicacaoPOO.Dominio/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacaoPOO.Dominio
+{
+    public class Cliente
+    {
+        string Nome { get; set; }
+        string CPF { get; set; }
+        Debito Debito { get; set; }
+        Credito credito { get; set; }
+
+    }
+}
---
+++ server/AplicacaoPOO.Dominio/Credito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacaoPOO.Dominio
+{
+    public class Credito
+    {
+        Pagamento Pagamento { get; set; }
+        int Parcelas { get; set; }
+        float Juros { get; set; }
+
+
+    }
+}
