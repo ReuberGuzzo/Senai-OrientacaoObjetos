4dc0321b119c4c3aba6d3a7801cf9f7fc1fdb822
Reuber Guzzo <reuber.rg@gmail.com>
2022-10-06 22:02:21 -0300

    foi feito um frmEmail

---
+++ client/AplicacaoPoo.Estrutural.Windows/frmSendMail.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace AplicacaoPoo.Estrutural.Windows
+{
+    partial class frmSendMail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMensagem = new System.Windows.Forms.RichTextBox();
+            this.txtAssunto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEnviarMensagem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Location = new System.Drawing.Point(12, 63);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(510, 23);
+            this.txtNomeCompleto.TabIndex = 0;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 110);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(510, 23);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Suporte e Contato";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome Completo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "E-mail";
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(12, 187);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(510, 84);
+            this.txtMensagem.TabIndex = 5;
+            this.txtMensagem.Text = "";
+            // 
+            // txtAssunto
+            // 
+            this.txtAssunto.Location = new System.Drawing.Point(12, 158);
+            this.txtAssunto.Name = "txtAssunto";
+            this.txtAssunto.Size = new System.Drawing.Size(509, 23);
+            this.txtAssunto.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Assunto";
+            // 
+            // btnEnviarMensagem
+            // 
+            this.btnEnviarMensagem.Location = new System.Drawing.Point(207, 282);
+            this.btnEnviarMensagem.Name = "btnEnviarMensagem";
+            this.btnEnviarMensagem.Size = new System.Drawing.Size(141, 23);
+            this.btnEnviarMensagem.TabIndex = 8;
+            this.btnEnviarMensagem.Text = "Enviar Mensagem";
+            this.btnEnviarMensagem.UseVisualStyleBackColor = true;
+            this.btnEnviarMensagem.Click += new System.EventHandler(this.btnEnviarMensagem_Click);
+            // 
+            // frmSendMail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 315);
+            this.Controls.Add(this.btnEnviarMensagem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAssunto);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNomeCompleto);
+            this.Name = "frmSendMail";
+            this.Text = "frmSendMail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtNomeCompleto;
+        private TextBox txtEmail;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private RichTextBox txtMensagem;
+        private TextBox txtAssunto;
+        private Label label4;
+        private Button btnEnviarMensagem;
+    }
+}
